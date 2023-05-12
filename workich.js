1f9881cb9abb593a427f10296d0a1e9413eefeb8
Ginrall <120490616+Ginrall@users.noreply.github.com>
2023-05-12 12:31:06 +0300

    Add files via upload

---
+++ workich.js
@@ -0,0 +1,24 @@
+function Click(){
+    document.getElementById("form").innerHTML = "<h1>logged</h1>";
+}
+function Click2(){
+    document.getElementById("form").innerHTML = "<h1>wait for a delivery</h1>";
+}
+function test(){
+    window.location.href = "delivery.html";
+}
+function Main(){
+    window.location.href = "login.html";
+}
+function Bakery(){
+    window.location.href = "bakery.html";
+}
+function Bake(){
+    window.location.href = "bake.html";
+}
+function Cakes(){
+    window.location.href = "cakes.html";
+}
+function Tarts(){
+    window.location.href = "tarts.html";
+}
